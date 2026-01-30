7f811e152449959facbff2449b025a89ddb5a207
chantip-pi <chantip.pi@ku.th>
2026-01-30 16:59:48 +0700

    additional route to appointment

--- routes/appointment.js
+++ routes/appointment.js
@@ -6,21 +6,25 @@ const {
   getAppointmentsByPatientId,
   getAppointmentsByDoctorId,
   getUpcomingAppointmentDate,
+  getScheduledAppointmentsByPatientId,
   addAppointment,
-  editAppointment,
-  removeAppointment
+  updateAppointment,
+  removeAppointment,
+  cancelAppointment,
 } = require('../controllers/appointment');
 
 const router = express.Router();
 
 router.get('/appointments', listAllAppointments);
 router.get('/appointments/upcoming/:patientId', getUpcomingAppointmentDate);
+router.get('/appointments/scheduled/:patientId', getScheduledAppointmentsByPatientId);
 router.get('/appointments/date/:appointmentDate', getAppointmentsByDate);
 router.get('/appointments/patient/:patientId', getAppointmentsByPatientId);
 router.get('/appointments/doctor/:doctorId', getAppointmentsByDoctorId);
 router.get('/appointments/:appointmentId', getAppointmentById);
 router.post('/appointments', addAppointment);
-router.put('/appointments/:appointmentId', editAppointment);
+router.put('/appointments/:appointmentId', updateAppointment);
+router.put('/appointments/:appointmentId/cancel', cancelAppointment);
 router.delete('/appointments/:appointmentId', removeAppointment);
 
 module.exports = router;
